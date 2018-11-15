f71fc42b19160d83a5e1a53994e42f8e2374afbf
tomkasp@gmail.com <tomkasp@gmail.com>
2018-11-15 06:40:18 +0100

    Initial commit

---
+++ src/main/java/com/athleticspot/neo4jplayground/Neo4jPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.athleticspot.neo4jplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jPlaygroundApplication.class, args);
+    }
+}
---
+++ src/test/java/com/athleticspot/neo4jplayground/Neo4jPlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package com.athleticspot.neo4jplayground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Neo4jPlaygroundApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
